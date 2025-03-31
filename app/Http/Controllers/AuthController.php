35d721810a6fdba98940a2a326efb29bfd8f68ca
Itamar Junior <itamar.junior@irriga.net>
2025-03-31 17:03:58 -0300

    feat: Criei o controller de login.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,7 +62,6 @@ class AuthController extends Controller
             ->get();
 
             $array['user']['properties'] = $properties;
-            return response()->json($array);
 
         } else {
             $array['error'] = $validator->errors()->first();
@@ -70,5 +69,47 @@ class AuthController extends Controller
         }
 
         return $array;
+    }
+
+    public function login(Request $request) {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'cpf' => 'required|digits:11',
+            'password' => 'required'
+        ]);
+
+        if(!$validator->fails()) {
+            $cpf = $request->input('cpf');
+            $password = $request->input('password');
+
+            $token = auth()->attempt([
+                'cpf' => $cpf,
+                'password' => $password
+            ]);
+
+            if(!$token) {
+                $array['error'] = 'CPF ou senha inválidos.';
+                return $array;
+            }
+
+            $array['token'] = $token;
+
+            $user = auth()->user();
+            $array['user'] = $user;
+
+            $properties = Unit::select(['id', 'name'])
+            ->where('id_owner', $user['id'])
+            ->get();
+
+            $array['user']['properties'] = $properties;
+
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+
     }
 }
